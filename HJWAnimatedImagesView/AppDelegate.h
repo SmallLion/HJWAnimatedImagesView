2237a9c6c930ede94ef85f277fb686fb03fb876b
自己 <Lemon@lion-lemondemacbook-pro.local>
2016-08-19 13:37:49 +0800

    Initial Commit

---
+++ HJWAnimatedImagesView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJWAnimatedImagesView
+//
+//  Created by Lion_Lemon on 16/8/19.
+//  Copyright © 2016年 Lion_Lemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJWAnimatedImagesView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJWAnimatedImagesView
+//
+//  Created by Lion_Lemon on 16/8/19.
+//  Copyright © 2016年 Lion_Lemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
